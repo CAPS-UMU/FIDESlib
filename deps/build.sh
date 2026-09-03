63a9d1f427d1db388d718e37072dc9c69040ab65
carlosad <carlos0105ad@gmail.com>
2026-09-03 09:47:49 +0200

    Update install

--- deps/build.sh
+++ deps/build.sh
@@ -14,7 +14,7 @@ git submodule update --init --recursive --remote
 #Source submodule.
 cd openfhe-src
 #git checkout fideslib-ref-v1.5.1.4
-git checkout fideslib-PS-chebychev-refactor
+git checkout fideslib-v2.1.4
 #git config user.email "FIDESlib"
 #git config user.name "FIDESlib"
 #git apply ../fideslib-ref-1.5.1.4.patch
